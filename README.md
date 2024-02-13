f87a61a24ec65a0c3e21d81413ce12f26e75198c
Chloramine <40995784+Chloramine@users.noreply.github.com>
2024-02-12 18:59:39 -0800

    get bot up and running

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cs2-nades-bot
 discord bot for cs2 nades and such
+
+nades data pulled from user submissions on https://csgonades.com
---
+++ main.py
@@ -0,0 +1,19 @@
+import os;      import discord
+from discord.ext import commands
+
+token = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix = '?', intents = intents)
+
+@bot.event
+async def on_ready():
+    print(f'logged in as {bot.user}')
+
+''' @bot.event
+async def on_message(message):
+    if bot.user.mentioned_in(message):
+''' # method for allowing pings to pass as a command
+    
+bot.run(token)
